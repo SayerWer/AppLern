b81723a61fa9f9f3fa3732a86a173b53a380c9e4
sawyer hill <sawyer hill>
2017-04-03 14:18:51 -0500

    changed second view

--- app/src/main/java/com/example/werprojects/apptstrmk/TstAct.java
+++ app/src/main/java/com/example/werprojects/apptstrmk/TstAct.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ScrollView;
 
 public class TstAct extends AppCompatActivity {
 
@@ -16,6 +17,7 @@ public class TstAct extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tst);
+        ScrollView scv = (ScrollView) findViewById(R.id.VVV);
 
     }
 }
